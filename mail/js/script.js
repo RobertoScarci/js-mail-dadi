0b793eed7d4e826418468673ad8754485f5f02f7
RobertoScarci <robertoscarci.02@gmail.com>
2023-11-15 15:06:17 +0100

    creazione della cartella mail e dei fogli appositi

---
+++ mail/js/script.js
@@ -0,0 +1,15 @@
+// individuo una lista di mail che possono accedere
+const listaMail = [
+    'pierino@gmail.com',
+    'matilde6mitica@gmail.com',
+    'franceschino@gmail.com',
+    'oronzocanà@gmail.com',
+    'lebronthegoat@gmail.com',
+    'chefcurry@gmail.com',
+    'maninblack@gmail.com',
+    'imyourfather@gmail.com',
+    'youwerethechoosenone@gmail.com'
+]
+
+// chiedo all'utente la sua mail personale per poterla verificare
+const userMail = document.querySelector('input')
