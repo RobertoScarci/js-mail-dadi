41b527142b692856fa4097522706b05b4cf25e2e
RobertoScarci <robertoscarci.02@gmail.com>
2023-11-15 15:26:26 +0100

    aggiunta della scritta nel html quando si va a inviare la mail appena inserita

--- mail/js/script.js
+++ mail/js/script.js
@@ -14,14 +14,15 @@ const listaMail = [
 // chiedo all'utente la sua mail personale per poterla verificare
 const userMail = document.querySelector('input')
 const button = document.querySelector('button')
+const final = document.querySelector('h2.word')
 
 button.addEventListener('click', function(){
     const mail = (userMail.value)
 
     if( listaMail.includes(mail)){
-        console.log('SI')
+        final.innerHTML = ('Puoi entrare!')
     } else {
-        console.log('NO')
+        final.innerHTML = ('Non puoi entrare!')
     }
 })
 
